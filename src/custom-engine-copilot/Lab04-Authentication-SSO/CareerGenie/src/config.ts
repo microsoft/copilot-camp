94384179fc13bae0ad460034dc88953648385a1c
Rabia Williams <rabiawilliams@gmail.com>
2024-08-12 10:20:10 +1000

    Auth code working

---
+++ src/custom-engine-copilot/Lab04-Authentication-SSO/CareerGenie/src/config.ts
@@ -0,0 +1,18 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  azureOpenAIKey: process.env.AZURE_OPENAI_API_KEY,
+  azureOpenAIEndpoint: process.env.AZURE_OPENAI_ENDPOINT,
+  azureOpenAIDeploymentName: process.env.AZURE_OPENAI_DEPLOYMENT_NAME,
+  azureOpenAIEmbeddingDeploymentName: process.env.AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME,
+  azureSearchKey: process.env.AZURE_SEARCH_KEY,
+  azureSearchEndpoint: process.env.AZURE_SEARCH_ENDPOINT,
+  indexName: process.env.INDEX_NAME,
+  aadAppClientId: process.env.AAD_APP_CLIENT_ID,
+  aadAppClientSecret: process.env.AAD_APP_CLIENT_SECRET,
+  aadAppOauthAuthorityHost: process.env.AAD_APP_OAUTH_AUTHORITY_HOST,
+  aadAppTenantId: process.env.AAD_APP_TENANT_ID,
+  botDomain: process.env.BOT_DOMAIN,
+};
+
+export default config;
