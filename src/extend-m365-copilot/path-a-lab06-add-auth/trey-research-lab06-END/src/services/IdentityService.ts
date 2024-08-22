0027ddea4013813d5e704c4266e363fb1f3f6772
Bob German <rogerman@microsoft.com>
2024-08-22 11:16:20 -0400

    Update IdentityService structure

--- src/extend-m365-copilot/path-a-lab06-add-auth/trey-research-lab06-END/src/services/IdentityService.ts
+++ src/extend-m365-copilot/path-a-lab06-add-auth/trey-research-lab06-END/src/services/IdentityService.ts
@@ -1,61 +1,119 @@
 import { HttpRequest } from "@azure/functions";
+import { HttpError } from './Utilities';
+import { Consultant } from '../model/baseModel';
+import { ApiConsultant } from '../model/apiModel';
 
 // This is a DEMO ONLY identity solution.
-// * With anonymous or API key access, we can't distinguish between users, so we will use
-//   the default consultant values.
-// * With Entra ID access via App Service middleware ("Easy Auth") we use the user's actual
-//   name and email but we will still use the rest of Consultant ID 1's data in the database.
-//   That way, everyone who runs the demo will share the same projects and other data.
+import { TokenValidator, ValidateTokenOptions, getEntraJwksUri } from 'jwt-validate';
+import ConsultantApiService from "./ConsultantApiService";
 
-const DEFAULT_CONSULTANT_ID = '1';
-const DEFAULT_CONSULTANT_NAME = 'Avery Howard';
-const DEFAULT_CONSULTANT_EMAIL = 'avery@treyresearch.com';
+interface UserInfo {
+    id: string;
+    name: string;
+    email: string;
+}
 
-let requestCounter = 0;             // Singleton to identify each request
-export default class Identity {
+class Identity {
+    private requestNumber = 1;  // Number the requests for logging purposes
 
-    public requestNumber: number;
-    public id: string;
-    public name: string;
-    public email: string;
-
-    constructor(req: HttpRequest) {
-
-        this.requestNumber = ++requestCounter;
-        this.id = DEFAULT_CONSULTANT_ID;
-
-        //#region DEBUG CODE
-        req.headers.forEach((value, name) => {
-            console.log(`🤯 Header: ${name} = ${value}`);
-        });
-        //#endregion
-
-        // Check for auth header from Easy Auth
-        let clientPrincipal = req.headers['x-ms-client-principal'];
-        // TEST TOKEN:  (uncomment this to test authenticated access locally)
-        // clientPrincipal = 'eyJhdXRoX3R5cCI6ImFhZCIsImNsYWltcyI6W3sidHlwIjoiYXVkIiwidmFsIjoiYXBpOlwvXC81MzA1Zjc0Ni05MDI0LTQ5YmMtODc4My1hMTY5MGM1OTE1NzgifSx7InR5cCI6ImlzcyIsInZhbCI6Imh0dHBzOlwvXC9zdHMud2luZG93cy5uZXRcLzU3ODcyNTQ4LTQ2YmQtNDU4ZS04YWU0LWMyYTU0MWVhNmQxYlwvIn0seyJ0eXAiOiJpYXQiLCJ2YWwiOiIxNzEyOTUzMDA1In0seyJ0eXAiOiJuYmYiLCJ2YWwiOiIxNzEyOTUzMDA1In0seyJ0eXAiOiJleHAiLCJ2YWwiOiIxNzEyOTU4MDY5In0seyJ0eXAiOiJodHRwOlwvXC9zY2hlbWFzLm1pY3Jvc29mdC5jb21cL2NsYWltc1wvYXV0aG5jbGFzc3JlZmVyZW5jZSIsInZhbCI6IjEifSx7InR5cCI6ImFpbyIsInZhbCI6IkFWUUFxXC84V0FBQUF0VWRZUmkyUWRVQW1MYklBbmFUQUpEXC9xTGhQZFYyUUlZVXgxY3l6RERZYXUzMklRT2czRmtHVmRpS05Mclg5MGRTenhNRytRdmRGOElLeTN1YnJPU2tTdmYyRVpibW90NUlFQ3dqWjF1V3M9In0seyJ0eXAiOiJodHRwOlwvXC9zY2hlbWFzLm1pY3Jvc29mdC5jb21cL2NsYWltc1wvYXV0aG5tZXRob2RzcmVmZXJlbmNlcyIsInZhbCI6InB3ZCJ9LHsidHlwIjoiaHR0cDpcL1wvc2NoZW1hcy5taWNyb3NvZnQuY29tXC9jbGFpbXNcL2F1dGhubWV0aG9kc3JlZmVyZW5jZXMiLCJ2YWwiOiJtZmEifSx7InR5cCI6ImFwcGlkIiwidmFsIjoiNTMwNWY3NDYtOTAyNC00OWJjLTg3ODMtYTE2OTBjNTkxNTc4In0seyJ0eXAiOiJhcHBpZGFjciIsInZhbCI6IjEifSx7InR5cCI6Imh0dHA6XC9cL3NjaGVtYXMueG1sc29hcC5vcmdcL3dzXC8yMDA1XC8wNVwvaWRlbnRpdHlcL2NsYWltc1wvc3VybmFtZSIsInZhbCI6Ikdlcm1hbiJ9LHsidHlwIjoiaHR0cDpcL1wvc2NoZW1hcy54bWxzb2FwLm9yZ1wvd3NcLzIwMDVcLzA1XC9pZGVudGl0eVwvY2xhaW1zXC9naXZlbm5hbWUiLCJ2YWwiOiJCb2IifSx7InR5cCI6ImlwYWRkciIsInZhbCI6IjIxNi4yMTIuMzIuMTI4In0seyJ0eXAiOiJuYW1lIiwidmFsIjoiQm9iIEdlcm1hbiJ9LHsidHlwIjoiaHR0cDpcL1wvc2NoZW1hcy5taWNyb3NvZnQuY29tXC9pZGVudGl0eVwvY2xhaW1zXC9vYmplY3RpZGVudGlmaWVyIiwidmFsIjoiODZhMWZjMjktODhlYi00MGU4LWFhZjYtYzgwNGNiODhmMDVjIn0seyJ0eXAiOiJyaCIsInZhbCI6IjAuQVZrQVNDV0hWNzFHamtXSzVNS2xRZXB0RzBiM0JWTWtrTHhKaDRPaGFReFpGWGlkQUljLiJ9LHsidHlwIjoiaHR0cDpcL1wvc2NoZW1hcy5taWNyb3NvZnQuY29tXC9pZGVudGl0eVwvY2xhaW1zXC9zY29wZSIsInZhbCI6InVzZXJfaW1wZXJzb25hdGlvbiJ9LHsidHlwIjoiaHR0cDpcL1wvc2NoZW1hcy54bWxzb2FwLm9yZ1wvd3NcLzIwMDVcLzA1XC9pZGVudGl0eVwvY2xhaW1zXC9uYW1laWRlbnRpZmllciIsInZhbCI6ImxSd2NTN2hxTDJrV0l5WlMtWVVKS2hJSE8wWF9YT0k2ZDV3eGs4YjAwUTAifSx7InR5cCI6Imh0dHA6XC9cL3NjaGVtYXMubWljcm9zb2Z0LmNvbVwvaWRlbnRpdHlcL2NsYWltc1wvdGVuYW50aWQiLCJ2YWwiOiI1Nzg3MjU0OC00NmJkLTQ1OGUtOGFlNC1jMmE1NDFlYTZkMWIifSx7InR5cCI6Imh0dHA6XC9cL3NjaGVtYXMueG1sc29hcC5vcmdcL3dzXC8yMDA1XC8wNVwvaWRlbnRpdHlcL2NsYWltc1wvbmFtZSIsInZhbCI6ImJvYkBsb3djb2RlcmV2b2x1dGlvbi5vbm1pY3Jvc29mdC5jb20ifSx7InR5cCI6Imh0dHA6XC9cL3NjaGVtYXMueG1sc29hcC5vcmdcL3dzXC8yMDA1XC8wNVwvaWRlbnRpdHlcL2NsYWltc1wvdXBuIiwidmFsIjoiYm9iQGxvd2NvZGVyZXZvbHV0aW9uLm9ubWljcm9zb2Z0LmNvbSJ9LHsidHlwIjoidXRpIiwidmFsIjoiNlU5T2RPLTJPVUs0MnRYZ1E5cERBQSJ9LHsidHlwIjoidmVyIiwidmFsIjoiMS4wIn1dLCJuYW1lX3R5cCI6Imh0dHA6XC9cL3NjaGVtYXMueG1sc29hcC5vcmdcL3dzXC8yMDA1XC8wNVwvaWRlbnRpdHlcL2NsYWltc1wvbmFtZSIsInJvbGVfdHlwIjoiaHR0cDpcL1wvc2NoZW1hcy5taWNyb3NvZnQuY29tXC93c1wvMjAwOFwvMDZcL2lkZW50aXR5XC9jbGFpbXNcL3JvbGUifQ==';
-        if (clientPrincipal) {
-            const clientPrincipalObj = JSON.parse(Buffer.from(clientPrincipal, 'base64').toString('utf8'));
-            this.name = clientPrincipalObj.claims.find((c: any) => c.typ === 'name')?.val;
-            this.email = clientPrincipalObj.claims.find((c: any) => c.typ === 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn')?.val;
-            console.log(`🔐 Request ${this.requestNumber} authenticated as ${this.name} (${this.email})`);
-        } else {
-            this.name = DEFAULT_CONSULTANT_NAME;
-            this.email = DEFAULT_CONSULTANT_EMAIL;
-            console.log(`⭐ Request ${this.requestNumber}`);
+    public async validateAndGetConsultantForUser(req: HttpRequest): Promise<ApiConsultant> {
+        const userInfo = await this.validateRequest(req);
+        let consultant = await ConsultantApiService.getApiConsultantById(userInfo.id);
+        if (!consultant) {
+            consultant = await this.createConsultantForUser(userInfo);
         }
+
+        return consultant;
     }
 
-    // Given a consultant name from Copilot, if the consultant is the same as the logged
-    // in user, translate to the default consultant name
-    public getDbConsultantName(apiConsultantName: string): string {
-        if (this.name.toLowerCase().indexOf(apiConsultantName.toLowerCase()) < 0) {
-            return apiConsultantName;
-        } else {
-            return DEFAULT_CONSULTANT_NAME;
+    public async validateRequest(req: HttpRequest): Promise<UserInfo> {
+        // Anonymous version:
+        return {
+            id: "1",
+            name: "Avery Howard",
+            email: "avery@treyresearch.com"
         }
+
+        // Auth version:
+        // try {
+            // const token = req.headers.get("Authorization")?.split(" ")[1];
+            // if (!token) {
+            //     throw new HttpError(404, "Assignment not found");
+            // }
+
+            // // create a new token validator for the Microsoft Entra common tenant
+            // const entraJwksUri = await getEntraJwksUri();
+            // const validator = new TokenValidator({
+            //     jwksUri: entraJwksUri
+            // });
+
+            // // Use these options for single-tenant applications
+            // const API_CLIENT_ID = '2f7a0770-4301-4dc3-b812-6315b8d51379';
+            // const API_TENANT_ID = '883a30d4-ca91-4cbb-b025-9c0f6d7820a0';
+            // const options: ValidateTokenOptions = {
+            //     audience: API_CLIENT_ID,
+            //     issuer: `https://login.microsoftonline.com/${API_TENANT_ID}/v2.0`,
+            //     scp: ["access_as_user"]
+            // };
+
+            // // Use these options for multi-tenant applications
+            // // const options: ValidateTokenOptions = {
+            // //   audience: process.env["AAD_APP_CLIENT_ID"],
+            // //   issuer: `https://login.microsoftonline.com/${process.env["AAD_APP_TENANT_ID"]}/v2.0`,
+            // //   // You need to manage the list of allowed tenants on your own!
+            // //   // For this sample, we only allow the tenant that the app is registered in
+            // //   allowedTenants: [process.env["AAD_APP_TENANT_ID"]],
+            // //   scp: ["access_as_user"]
+            // // };
+
+
+            // // validate the token
+            // const validToken = await validator.validateToken(token, options);
+            // console.log(`Request ${this.requestNumber++}: Token is valid for user ${validToken.preferred_username} (${validToken.name})`);
+
+            // return {
+            //     id: validToken.oid,
+            //     name: validToken.name,
+            //     email: validToken.upn
+            // };
+        // }
+        // catch (ex) {
+        //     // Token is missing or invalid - return a 401 error
+        //     console.error(ex);
+        //     throw new HttpError(404, "Unauthorized");
+        // }
+    }
+
+    private async createConsultantForUser(userInfo: UserInfo): Promise<ApiConsultant> {
+        
+        // Create a new consultant record for this user with default values
+        const consultant: Consultant = {
+            id: userInfo.id,
+            name: userInfo.name,
+            email: userInfo.email,
+            phone: "1-555-123-4567",
+            consultantPhotoUrl: "https://bobgerman.github.io/fictitiousAiGenerated/Avery.jpg",
+            location: {
+                street: "One Memorial Drive",
+                city: "Cambridge",
+                state: "MA",
+                country: "USA",
+                postalCode: "02142",
+                latitude: 42.361366,
+                longitude: -71.081257,
+                mapUrl: ""
+            },
+            skills: [ "JavaScript", "TypeScript" ],
+            certifications: [ "Azure Development" ],
+            roles: [ "Architect", "Project Lead" ]
+        };
+        const result = await ConsultantApiService.createApiConsultant(consultant);
+        return result;
     }
 }
+
+export default new Identity();
+
 
 
 
