46fa9ba983f3985923491cd5bd22bea2d6c11b35
Bob German <rogerman@microsoft.com>
2024-08-22 15:44:54 -0400

    Token validation works

--- src/extend-m365-copilot/path-a-lab06-add-auth/trey-research-lab06-END/src/services/IdentityService.ts
+++ src/extend-m365-copilot/path-a-lab06-add-auth/trey-research-lab06-END/src/services/IdentityService.ts
@@ -28,60 +28,60 @@ class Identity {
 
     public async validateRequest(req: HttpRequest): Promise<UserInfo> {
         // Anonymous version:
-        return {
-            id: "1",
-            name: "Avery Howard",
-            email: "avery@treyresearch.com"
-        }
+        // return {
+        //     id: "1",
+        //     name: "Avery Howard",
+        //     email: "avery@treyresearch.com"
+        // }
 
         // Auth version:
-        // try {
-            // const token = req.headers.get("Authorization")?.split(" ")[1];
-            // if (!token) {
-            //     throw new HttpError(404, "Assignment not found");
-            // }
+        try {
+            const token = req.headers.get("Authorization")?.split(" ")[1];
+            if (!token) {
+                throw new HttpError(404, "Assignment not found");
+            }
 
-            // // create a new token validator for the Microsoft Entra common tenant
-            // const entraJwksUri = await getEntraJwksUri();
-            // const validator = new TokenValidator({
-            //     jwksUri: entraJwksUri
-            // });
+            // create a new token validator for the Microsoft Entra common tenant
+            const entraJwksUri = await getEntraJwksUri();
+            const validator = new TokenValidator({
+                jwksUri: entraJwksUri
+            });
 
-            // // Use these options for single-tenant applications
-            // const API_CLIENT_ID = '2f7a0770-4301-4dc3-b812-6315b8d51379';
-            // const API_TENANT_ID = '883a30d4-ca91-4cbb-b025-9c0f6d7820a0';
+            // Use these options for single-tenant applications
+            const API_CLIENT_ID = '2f7a0770-4301-4dc3-b812-6315b8d51379';
+            const API_TENANT_ID = '883a30d4-ca91-4cbb-b025-9c0f6d7820a0';
+            const options: ValidateTokenOptions = {
+                audience: `api://${API_CLIENT_ID}`,
+                issuer: `https://sts.windows.net/${API_TENANT_ID}/`,
+                scp: ["access_as_user"]
+            };
+
+            // Use these options for multi-tenant applications
             // const options: ValidateTokenOptions = {
-            //     audience: API_CLIENT_ID,
-            //     issuer: `https://login.microsoftonline.com/${API_TENANT_ID}/v2.0`,
-            //     scp: ["access_as_user"]
+            //   audience: process.env["AAD_APP_CLIENT_ID"],
+            //   issuer: `https://login.microsoftonline.com/${process.env["AAD_APP_TENANT_ID"]}/v2.0`,
+            //   // You need to manage the list of allowed tenants on your own!
+            //   // For this sample, we only allow the tenant that the app is registered in
+            //   allowedTenants: [process.env["AAD_APP_TENANT_ID"]],
+            //   scp: ["access_as_user"]
             // };
 
-            // // Use these options for multi-tenant applications
-            // // const options: ValidateTokenOptions = {
-            // //   audience: process.env["AAD_APP_CLIENT_ID"],
-            // //   issuer: `https://login.microsoftonline.com/${process.env["AAD_APP_TENANT_ID"]}/v2.0`,
-            // //   // You need to manage the list of allowed tenants on your own!
-            // //   // For this sample, we only allow the tenant that the app is registered in
-            // //   allowedTenants: [process.env["AAD_APP_TENANT_ID"]],
-            // //   scp: ["access_as_user"]
-            // // };
 
+            // validate the token
+            const validToken = await validator.validateToken(token, options);
+            console.log(`Request ${this.requestNumber++}: Token is valid for user ${validToken.preferred_username} (${validToken.name})`);
 
-            // // validate the token
-            // const validToken = await validator.validateToken(token, options);
-            // console.log(`Request ${this.requestNumber++}: Token is valid for user ${validToken.preferred_username} (${validToken.name})`);
-
-            // return {
-            //     id: validToken.oid,
-            //     name: validToken.name,
-            //     email: validToken.upn
-            // };
-        // }
-        // catch (ex) {
-        //     // Token is missing or invalid - return a 401 error
-        //     console.error(ex);
-        //     throw new HttpError(404, "Unauthorized");
-        // }
+            return {
+                id: validToken.oid,
+                name: validToken.name,
+                email: validToken.upn
+            };
+        }
+        catch (ex) {
+            // Token is missing or invalid - return a 401 error
+            console.error(ex);
+            throw new HttpError(404, "Unauthorized");
+        }
     }
 
     private async createConsultantForUser(userInfo: UserInfo): Promise<ApiConsultant> {
