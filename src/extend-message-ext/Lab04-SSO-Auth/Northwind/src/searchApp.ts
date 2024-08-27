386b54c9beaaded85bb687228fd0a05f814ebf6f
Rabia Williams <rabiawilliams@gmail.com>
2024-08-28 09:40:35 +1000

    added new app

---
+++ src/extend-message-ext/Lab04-SSO-Auth/Northwind/src/searchApp.ts
@@ -0,0 +1,69 @@
+import {
+  TeamsActivityHandler,
+  TurnContext,
+  MessagingExtensionQuery,
+  MessagingExtensionResponse,
+  InvokeResponse,
+  AdaptiveCardInvokeResponse
+} from "botbuilder";
+import productSearchCommand from "./messageExtensions/productSearchCommand";
+import discountedSearchCommand from "./messageExtensions/discountSearchCommand";
+import actionHandler from "./adaptiveCards/cardHandler";
+import { CreateActionErrorResponse } from "./adaptiveCards/utils";
+import customerSearchCommand from "./messageExtensions/customerSearchCommand";
+
+
+export class SearchApp extends TeamsActivityHandler {
+  constructor() {
+    super();
+  }
+
+  // Handle search message extension
+  public async handleTeamsMessagingExtensionQuery(
+    context: TurnContext,
+    query: MessagingExtensionQuery
+  ): Promise<MessagingExtensionResponse> {
+
+    switch (query.commandId) {
+      case productSearchCommand.COMMAND_ID: {
+        return productSearchCommand.handleTeamsMessagingExtensionQuery(context, query);
+      }
+      case discountedSearchCommand.COMMAND_ID: {
+        return discountedSearchCommand.handleTeamsMessagingExtensionQuery(context, query);
+      }
+      case customerSearchCommand.COMMAND_ID: {
+        return customerSearchCommand.handleTeamsMessagingExtensionQuery(context, query);
+      }
+    }
+
+  }
+
+  // Handle adaptive card actions
+  public async onAdaptiveCardInvoke(context: TurnContext): Promise<AdaptiveCardInvokeResponse>  {
+    try {     
+   
+        switch (context.activity.value.action.verb) {
+          case 'ok': {
+            return actionHandler.handleTeamsCardActionUpdateStock(context);
+          }
+          case 'restock': {
+            return actionHandler.handleTeamsCardActionRestock(context);
+          }
+          case 'cancel': {
+            return actionHandler.handleTeamsCardActionCancelRestock(context);
+          }
+          case 'refresh': {
+            return actionHandler.handleTeamsCardActionRefresh(context);
+          }
+          default: {
+            console.log ('Unknown Invoke activity received');
+            return CreateActionErrorResponse(400, 0, `ActionVerbNotSupported: ${context.activity.value.action.verb} is not a supported action verb.`);
+          }
+        }
+     
+    } catch (err) {
+      return CreateActionErrorResponse(500, 0, err.message);
+    } 
+  }
+}
+
